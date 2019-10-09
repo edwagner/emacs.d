a95b6ccbf54be96752bd6375b8a01b9e9a41548c
Ed Wagner <ed@edwagner.org>
2019-10-09 16:38:43 -0500

    Set font and window size
    Use Office Code Pro and set default height and width

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,5 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
+(add-to-list 'default-frame-alist '(font . "Office Code Pro-14"))
+(add-to-list 'default-frame-alist '(height . 60))
+(add-to-list 'default-frame-alist '(width . 120))
+
 (provide 'init-local)
 ;;; init-local.el ends here
